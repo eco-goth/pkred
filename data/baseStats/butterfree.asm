f64b48fae7f525b1afd303bed47fbf534205f32b
eco-goth <152532122+eco-goth@users.noreply.github.com>
2023-12-22 08:49:46 -0600

    Update butterfree.asm

--- data/baseStats/butterfree.asm
+++ data/baseStats/butterfree.asm
@@ -2,8 +2,8 @@ db DEX_BUTTERFREE ; pokedex id
 db 60 ; base hp
 db 45 ; base attack
 db 50 ; base defense
-db 70 ; base speed
-db 80 ; base special
+db 80 ; base speed
+db 90 ; base special
 db BUG ; species type 1
 db FLYING ; species type 2
 db 45 ; catch rate
