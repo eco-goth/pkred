e75aefaf6ba32e8b9b00d4971a735080a12e9a47
eco-goth <152532122+eco-goth@users.noreply.github.com>
2023-12-24 00:39:44 -0600

    Update route22.asm

--- data/wildPokemon/route22.asm
+++ data/wildPokemon/route22.asm
@@ -3,10 +3,10 @@ Route22Mons:
 	db 3,NIDORAN_M
 	db 3,NIDORAN_F
 	db 3,SPEAROW
-	db 3,POLIWAG
+	db 5,SPEAROW
 	db 4,NIDORAN_F
 	db 4,NIDORAN_M
-	db 5,SPEAROW
+	db 3,POLIWAG
 	db 4,POLIWAG
 	db 4,MANKEY
 	db 6,MANKEY
