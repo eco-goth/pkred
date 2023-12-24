bd1cbc65304d675a8f2894c926b05a69ab03dae9
eco-goth <152532122+eco-goth@users.noreply.github.com>
2023-12-23 22:42:59 -0600

    Update route1.asm

--- data/wildPokemon/route1.asm
+++ data/wildPokemon/route1.asm
@@ -1,7 +1,7 @@
 Route1Mons:
 	db $19
 	db 3,PIDGEY
-	db 3,RATTATA
+	db 3,SPEAROW
 	db 3,RATTATA
 	db 2,RATTATA
 	db 2,PIDGEY
@@ -9,5 +9,5 @@ Route1Mons:
 	db 3,PIDGEY
 	db 4,RATTATA
 	db 4,PIDGEY
-	db 5,PIDGEY
+	db 5,SPEAROW
 	db $00
