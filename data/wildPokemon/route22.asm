6b74f443662c20937720cb673db85b5dd4b18487
eco-goth <152532122+eco-goth@users.noreply.github.com>
2023-12-23 22:59:34 -0600

    Update route22.asm

--- data/wildPokemon/route22.asm
+++ data/wildPokemon/route22.asm
@@ -1,13 +1,13 @@
 Route22Mons:
 	db $19
-		db 3,RATTATA
-		db 3,NIDORAN_M
-		db 4,RATTATA
-		db 4,NIDORAN_M
-		db 2,RATTATA
-		db 2,NIDORAN_M
-		db 3,SPEAROW
-		db 5,SPEAROW
-		db 3,NIDORAN_F
-		db 4,NIDORAN_F
+	db 3,NIDORAN_M
+	db 3,NIDORAN_F
+	db 3,SPEAROW
+	db 3,POLIWAG
+	db 4,NIDORAN_F
+	db 4,NIDORAN_M
+	db 5,SPEAROW
+	db 4,POLIWAG
+	db 4,MANKEY
+	db 6,MANKEY
 	db $00
