c4963968dd08e9515d7c7708ebfb43c977fcec5c
eco-goth <152532122+eco-goth@users.noreply.github.com>
2023-12-24 14:14:17 -0600

    Update meowth.asm

--- data/baseStats/meowth.asm
+++ data/baseStats/meowth.asm
@@ -2,7 +2,7 @@ db DEX_MEOWTH ; pokedex id
 db 40 ; base hp
 db 45 ; base attack
 db 35 ; base defense
-db 90 ; base speed
+db 95 ; base speed
 db 40 ; base special
 db NORMAL ; species type 1
 db NORMAL ; species type 2
