0541ad84276182e83af19219634ff2df67fdeb7d
eco-goth <152532122+eco-goth@users.noreply.github.com>
2023-12-24 16:15:37 -0600

    Update mtmoon1.asm

--- data/wildPokemon/mtmoon1.asm
+++ data/wildPokemon/mtmoon1.asm
@@ -2,12 +2,12 @@ MoonMons1:
 	db $0A
 	db 8,ZUBAT
 	db 9,ZUBAT
-	db 10,ZUBAT
-	db 6,GEODUDE
-	db 7,ZUBAT
-	db 10,ZUBAT
 	db 10,GEODUDE
-	db 11,PARAS
-	db 12,ZUBAT
+	db 6,ZUBAT
+	db 7,ZUBAT
+	db 10,SANDSHREW
+	db 10,GEODUDE
+	db 11,SANDSHREW
+	db 12,GEODUDE
 	db 12,CLEFAIRY
 	db $00
