43d23a175d0d6fdb156e1953b0cdc64f014e666e
eco-goth <152532122+eco-goth@users.noreply.github.com>
2023-12-30 15:43:35 -0600

    Update lickitung.asm

--- data/baseStats/lickitung.asm
+++ data/baseStats/lickitung.asm
@@ -12,9 +12,9 @@ INCBIN "pic/swmon/lickitung.pic",0,1 ; 77, sprite dimensions
 dw LickitungPicFront
 dw LickitungPicBack
 ; attacks known at lvl 0
-db WRAP
+db POUND
 db SUPERSONIC
-db 0
+db LICK
 db 0
 db 0 ; growth rate
 ; learnset
