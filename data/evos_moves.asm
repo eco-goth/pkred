3a26d1b5d4f6bd765a3e37c4bed78ea8e9dc015f
eco-goth <152532122+eco-goth@users.noreply.github.com>
2023-12-30 14:39:41 -0600

    Update evos_moves.asm

--- data/evos_moves.asm
+++ data/evos_moves.asm
@@ -1334,13 +1334,14 @@ WigglytuffEvosMoves:
 ; Evolutions
 	db 0
 ; Learnset
-	db 9, POUND
-	db 14, DISABLE
-	db 19, DEFENSE_CURL
-	db 24, DOUBLESLAP
-	db 29, REST
-	db 34, BODY_SLAM
-	db 39, DOUBLE_EDGE
+	db 10, POUND
+	db 11, MIMIC
+	db 13, REST
+	db 16, DOUBLESLAP
+	db 20, DISABLE
+	db 25, COMET_PUNCH
+	db 31, BODY_SLAM
+	db 38, DOUBLE_EDGE
 	db 0
 
 EeveeEvosMoves:
