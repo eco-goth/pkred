0c46ac51e15a8a8ba2ab34c5a5380e19ed86390f
eco-goth <152532122+eco-goth@users.noreply.github.com>
2024-01-01 19:28:51 -0600

    Update moves.asm

--- data/moves.asm
+++ data/moves.asm
@@ -31,7 +31,7 @@ MoveEnd:
 	move WHIRLWIND,    SWITCH_AND_TELEPORT_EFFECT,   0, NORMAL,    100, 20
 	move FLY,          FLY_EFFECT,                  70, FLYING,    95, 15
 	move BIND,         TRAPPING_EFFECT,             15, NORMAL,    75, 15
-	move SLAM,         NO_ADDITIONAL_EFFECT,        80, NORMAL,    75, 20
+	move SLAM,         NO_ADDITIONAL_EFFECT,        80, NORMAL,    100, 20
 	move VINE_WHIP,    NO_ADDITIONAL_EFFECT,        40, GRASS,    100, 10
 	move STOMP,        FLINCH_SIDE_EFFECT2,         65, NORMAL,   100, 20
 	move DOUBLE_KICK,  ATTACK_TWICE_EFFECT,         30, FIGHTING, 100, 30
