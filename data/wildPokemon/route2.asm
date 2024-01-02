fd5d58988669dffa4f8454c15f569b54c9a80ad1
eco-goth <152532122+eco-goth@users.noreply.github.com>
2024-01-01 20:01:56 -0600

    Update route2.asm

--- data/wildPokemon/route2.asm
+++ data/wildPokemon/route2.asm
@@ -1,13 +1,13 @@
 Route2Mons:
 	db $19
-	db 3,RATTATA
-	db 3,PIDGEY
-	db 4,RATTATA
-	db 4,MANKEY
-	db 5,MANKEY
+	db 5,RATTATA
 	db 5,PIDGEY
-	db 5,NIDORAN_M
-	db 5,NIDORAN_F
+	db 5,RATTATA
+	db 5,MANKEY
+	db 6,MANKEY
+	db 6,PIDGEY
+	db 6,NIDORAN_M
+	db 6,NIDORAN_F
 	db 7,PIDGEY
-	db 7,PIDGEY
+	db 9,PIDGEY
 	db $00
