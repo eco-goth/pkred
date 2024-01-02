a2b0f775084413b07ad81c66ca8ecbf02081bf81
eco-goth <152532122+eco-goth@users.noreply.github.com>
2024-01-01 19:42:51 -0600

    Update mtmoonb2.asm

--- data/wildPokemon/mtmoonb2.asm
+++ data/wildPokemon/mtmoonb2.asm
@@ -1,13 +1,13 @@
 MoonMonsB2:
 	db $0A
-	db 10,ZUBAT
-	db 11,GEODUDE
+	db 11,ZUBAT
+	db 12,GEODUDE
 	db 13,PARAS
-	db 11,ZUBAT
-	db 11,ZUBAT
 	db 12,ZUBAT
 	db 13,ZUBAT
-	db 11,CLEFAIRY
-	db 12,CLEFAIRY
+	db 13,ZUBAT
+	db 14,ZUBAT
+	db 13,CLEFAIRY
 	db 14,CLEFAIRY
+	db 15,CLEFAIRY
 	db $00
